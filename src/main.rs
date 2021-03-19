73ad6d4d98eb38adb9ef98d8ef8d9ff79af54301
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2021-03-19 22:41:13 +0300

    add allocator bp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::alloc::{System, GlobalAlloc, Layout};
+use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
+
+struct Counter;
+
+static ALLOCATED: AtomicUsize = AtomicUsize::new(0);
+
+unsafe impl GlobalAlloc for Counter {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let ret = System.alloc(layout);
+        if !ret.is_null() {
+            ALLOCATED.fetch_add(layout.size(), SeqCst);
+        }
+        return ret
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        System.dealloc(ptr, layout);
+        ALLOCATED.fetch_sub(layout.size(), SeqCst);
+    }
+}
+
+#[global_allocator]
+static A: Counter = Counter;
+
 fn main() {
-    println!("Hello, world!");
+    println!("allocated bytes before main: {}", ALLOCATED.load(SeqCst));
+
+    loop {
+        //let a = Box::new([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+        let a = Box::new(0);
+    }
 }
